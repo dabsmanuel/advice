556d7a4dd0904e052e54c8471a703acd567dac4d
dabsmanuel <dabsmanuel5015@gmail.com>
2025-03-30 13:23:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css' // Make sure to import the CSS file
 
-function App() {
+const App = () => {
+  const [data, setData] = useState('');
+  const [count, setCount] = useState(0)
+
+  async function getAdvice() {
+    const api = await fetch('https://api.adviceslip.com/advice');
+    const data = await api.json();
+    setData(data.slip.advice)
+    setCount((c) => c + 1);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="advice-container">
+        <h1>{data || 'Click the button to get some advice'}</h1>
+        <button className="advice-button" onClick={getAdvice}>Get Advice</button>
+      </div>
+      <div className="counter">
+        You have read <span className="counter-number">{count}</span> pieces of advice
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
